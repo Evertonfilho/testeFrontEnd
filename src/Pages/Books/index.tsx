052ac13f607a4756012e16120fc824037a8885d0
Everton Araujo <evertonfilho0513@gmail.com>
2022-10-29 15:51:28 -0300

    feat: Adicionando CardBooks e tipando BooksData .

--- src/Pages/Books/index.tsx
+++ src/Pages/Books/index.tsx
@@ -1,10 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Key, useState } from "react"
 import axios from "axios"
+import {CardBook, CardBooks, Container} from './styles' 
+
 
 type BooksData = {
   id:string,
-  volumeInfo:string
+  volumeInfo: {
+    title:string,
+    authors:string,
+    description:string,
+    imageLinks:{
+      thumbnail: HTMLElement,
+    }
+    
+  }
 }
 
 
@@ -16,7 +27,7 @@ export function Books(){
   
     const handleSearch = () =>{
       const keyApi = 'AIzaSyCEw67iaWQJyudnMlBrjhLnx9WvgnW18qU'
-      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}${inauthor}&key=${keyApi}`)
+      axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}${inauthor}&key=${keyApi}&maxResults=40`)
       .then(response => {
         setBooks(response.data.items)
         console.log(response.data)
@@ -25,7 +36,7 @@ export function Books(){
     }
   
     return(
-      <>
+      <Container>
       <h1>Google Books</h1>
       <input 
       type="text"
@@ -38,25 +49,40 @@ export function Books(){
       onChange={(e) => setInauthor(e.target.value)}
       />
       <button onClick={handleSearch} >Buscar</button>
+      <CardBooks>
       <ul>
         {books.map((book:any , index: Key | null | undefined) => {
-          return(
-            <div 
-            className="Items"
-            key={index}
-            >
-            <div className="Item">
-            <li  >
-              <span >{book.volumeInfo.title}</span>
-              <span > {book.volumeInfo.authors}</span>
-              <span > {book.volumeInfo.authors}</span>
-              <span > {book.volumeInfo.description}</span>
-            </li>
-            </div>
-            </div>
-          )
+          try{
+            return(
+              <CardBook key={index}>
+                <img src={book.volumeInfo.imageLinks.thumbnail} alt="Capa Livro" />
+                <span >{book.volumeInfo.title}</span>
+                <span > {book.volumeInfo.authors}</span>
+                <span > {book.volumeInfo.description}</span>          
+              </CardBook>
+            )
+          }
+          catch(err){
+            return(
+              <CardBook key={index}>
+                <img src="assets/semImagem.png" alt="Livro sem Imagem" />
+                <span >{book.volumeInfo.title}</span>
+                <span > {book.volumeInfo.authors}</span>
+                <span > {book.volumeInfo.description}</span>          
+              </CardBook>
+            )
+          }
+
+
+          
         })}
       </ul>
-      </>
+      </CardBooks>
+      </Container>
   )
 }
+
+/*
+
+
+      */
--- src/Pages/Books/styles.ts
+++ src/Pages/Books/styles.ts
@@ -1,4 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
+ display: flex;
+ flex-wrap: wrap;
+
+ width: 100%;
+ height: 100%;
+`
+
+export const CardBooks = styled.div`
+
+`
+
+export const CardBook = styled.div`
+
 `
